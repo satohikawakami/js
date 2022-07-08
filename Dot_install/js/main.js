a550b50ed0951ed3d4a922a5925948662582e3a3
github satoshikawakami <s.kawakami0816@gmail.com>
2022-07-08 22:35:04 +0900

    OK

--- Dot_install/js/main.js
+++ Dot_install/js/main.js
@@ -1,22 +1,11 @@
 'use strict';
 
 {
- 
-const scores = [10, 3 , 9];
-
-let sum = 0;
-
-scores.forEach(score => {
-  sum += score;
-}
-  );
-
-  const avg = sum / scores.length;
-
-  console.log(sum);
-  console.log(avg);
-
-  console.log(Math.floor(avg));
+  document.querySelector('ul').addEventListener('click', e =>{
+    if(e.target.nodeName === 'LI'){
+      e.target.classList.toggle('done');
+    }
+  });
 }
 
 
